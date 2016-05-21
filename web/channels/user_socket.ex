9963411f6be2731fdbd4eca6edbb6002bb776a78
Joe Letizia <joe@gust.com>
2016-05-20 23:11:19 -0400

    more heroku setup

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Blogger.UserSocket do
   # channel "rooms:*", Blogger.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
